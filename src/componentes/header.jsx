a86e4658d5b14205b17aacfeb65f0a8bdb351715
Ignacio <igna73083@gmail.como>
2023-05-30 21:30:26 -0300

    PreEntrega1Pacheco

---
+++ src/componentes/header.jsx
@@ -0,0 +1,22 @@
+
+
+
+
+const Header = () => {
+
+    return (
+        <header className="header">
+            <div className="header-div">
+                <h1 className="header-logo">MUNDO</h1>
+            </div>
+
+            <nav className="header-nav">
+                <a className="header-a" href="">Inicio</a>
+                <a className="header-a" href="">Paginas</a>
+                <a className="header-a" href="">Contacto</a>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
---
+++ src/componentes/productos.jsx
@@ -0,0 +1,14 @@
+
+
+const ItemProductos = ({mensaje}) => {
+    return (
+        <div className="list-container">
+            <h2>Productos</h2>
+            <hr />
+
+            <p>{mensaje}</p>
+        </div>
+    )
+}
+
+export default ItemProductos
